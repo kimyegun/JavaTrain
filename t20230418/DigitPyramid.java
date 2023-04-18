b4ac718517762b7ffa330ef8ed9141b9de9e25c2
kimyegun123@gmail.com <lmcomputer@daum.net>
2023-04-18 17:45:26 +0900

    숫자피라미드

---
+++ t20230418/DigitPyramid.java
@@ -0,0 +1,21 @@
+package t20230418;
+
+import java.util.Scanner;
+
+public class DigitPyramid {
+
+	public static void main(String[] args) {
+		Scanner stdIn = new Scanner(System.in);
+		System.out.println("숫자 피라미드를 표시합니다.");
+		System.out.print("단수는:");
+		int n = stdIn.nextInt();
+		for (int i = 1; i <=n; i++) {
+			for(int j = 1; j <=n - i; j++)
+				System.out.print(' ');
+			for(int j = 1; j <=2 * i - 1; j++)
+			System.out.print(i % 10);
+		System.out.println();
+	}
+	}
+
+}
