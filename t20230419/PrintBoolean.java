8465dabdd5ed7e48c436bdf40d51008138a6e331
kimyegun123@gmail.com <lmcomputer@daum.net>
2023-04-19 17:38:50 +0900

    논리형변수표시

---
+++ t20230419/PrintBoolean.java
@@ -0,0 +1,13 @@
+package t20230419;
+
+public class PrintBoolean {
+
+	public static void main(String[] args) {
+		boolean b1 = true;
+		boolean b2 = false;
+		
+		System.out.println("b1 =" + b1);
+		System.out.println("b2 =" + b2);
+
+	}
+}
